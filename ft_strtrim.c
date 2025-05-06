9eafc7195fccd27f640532029aa6d01574891e6c
Andrey <andreykosmenuk13@gmail.com>
2025-05-06 20:01:06 +0200

    Add ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,56 +6,38 @@
 /*   By: akosmeni <akosmeni@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 17:37:47 by akosmeni          #+#    #+#             */
-/*   Updated: 2025/05/02 14:21:05 by akosmeni         ###   ########.fr       */
+/*   Updated: 2025/05/06 19:59:20 by akosmeni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*str_trim;
-	unsigned char *s = (unsigned char *)s1;
-	char	*s1_copy;
+	int		start;
 	int		i;
-	int j;
-	int k;
+	int		end;
+	char	*res;
 
-	i = 0;
-	k = 0;
-	s1_copy = malloc(ft_strlen(s1) * (sizeof(char)) + 1);
-	if (!s1_copy)
-		return (NULL);
-	while (s1[i] != '\0')
-	{
-		j = 0;
-		while (set[j] != '\0')
-		{
-			if (s1[i] != set[j])
-			{
-				s[k] = s[k + 1];
-			}
-				
-			j++;
-		}
-		i++;
-		k++;
-	}
-	s[k] = '\0';
-	printf("%s", s);
-	return (s1_copy);
-}
-/*while (s1[i] != '\0')
-	{
-		more_start = start + 1;
-		while (set[j] != '\0')
-		{
-			if (*start != set[j])
-				more_start = start;
-			j++;
-		}
-		i++;
+	if (!set || !s1)
+		return (ft_strdup(s1));
+	start = 0;
+	end = ft_strlen(s1);
+	while (s1[start] != '\0' && ft_strchr(set, s1[start]) != NULL)
 		start++;
-	}*/
+	while (end > start && ft_strchr(set, s1[end - 1]) != NULL )
+		end--;
+	res = malloc(sizeof(char) * (end - start + 1));
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (start < end)
+	{
+		res[i] = s1[start];
+		start++;
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
--- main.c
+++ main.c
@@ -6,35 +6,20 @@
 /*   By: akosmeni <akosmeni@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 20:42:34 by akosmeni          #+#    #+#             */
-/*   Updated: 2025/05/05 12:17:18 by akosmeni         ###   ########.fr       */
+/*   Updated: 2025/05/06 19:49:30 by akosmeni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 #include <assert.h>
-#include <bsd/string.h>
-#include <bsd/stdlib.h>
 #include <limits.h>
 
 int main() {
-	/*char *s = "Helloshowsaresyou";
-	char ch = 's';
-	char *res[4] = {"hello","ejhd","dfds","rtre"};
-	for (int i = 0; res[i] != NULL; i++)
-	{
-		for (int j = 0; res[i][j] != '\0'; j++)
-		{
-			printf("%c", res[i][j]);
-		}
-		printf("\n");
-	}*/
+	char *s = ".Hel.,l.o,,,,";
+	char *ch = ",.";
 
-	int n = 0;
-
-	char *res = ft_itoa(n);
-	printf("Integer: %d, converts into string: %s\n", n, res);
-	
-	free(res);
+	char *res = ft_strtrim(s, ch);
+	printf("Result: %s\n", res);
     return 0;
 }
