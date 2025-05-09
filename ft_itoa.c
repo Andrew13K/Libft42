4ff7ad6cd2f4fb4fcb2c97b266ce7d7a200ee946
Andrey <andreykosmenuk13@gmail.com>
2025-05-09 17:07:32 +0200

    ft_itoa.c add

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,42 +6,89 @@
 /*   By: akosmeni <akosmeni@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 11:19:56 by akosmeni          #+#    #+#             */
-/*   Updated: 2025/05/06 11:20:09 by akosmeni         ###   ########.fr       */
+/*   Updated: 2025/05/09 17:06:50 by akosmeni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stddef.h>
-#include <stdio.h>
+#include <stdlib.h>
+
+static int	count_digits(int long num)
+{
+	int	count;
+
+	count = 0;
+	if (num == 0)
+		return (1);
+	if (num < 0)
+		num *= -1;
+	while (num > 0)
+	{
+		num /= 10;
+		count++;
+	}
+	return (count);
+}
+
+static char	*allocate_memory(int long num)
+{
+	char	*str;
+	int		count;
+
+	if (num < 0)
+	{
+		count = count_digits(num);
+		str = malloc((sizeof(char) * count) + 2);
+		if (str)
+			str[0] = '-';
+	}
+	else
+	{
+		count = count_digits(num);
+		str = malloc((sizeof(char) * count) + 1);
+	}
+	return (str);
+}
+
+static char	*fill_str(int long num, int i, int count, char *str)
+{
+	if (num < 0)
+	{
+		num *= -1;
+		while (i > 0)
+		{
+			str[i] = num % 10 + '0';
+			num /= 10;
+			i--;
+		}
+		str[count + 1] = '\0';
+		return (str);
+	}
+	i = count - 1;
+	while (i >= 0)
+	{
+		str[i] = num % 10 + '0';
+		num /= 10;
+		i--;
+	}
+	str[count] = '\0';
+	return (str);
+}
 
 char	*ft_itoa(int n)
 {
-	char *num;
-	char v;
-	int temp_n = n;
+	int			count;
+	int			i;
+	char		*str;
+	char		*res;
+	int long	num;
 
-	int i = 0;
-	if (n < 0)
-	{
-		num[0] = '-';
-		n *= -1;
-	}
-	while(temp_n > 0)
-	{
-		temp_n /= 10;
-		i++;
-	}
-	num = malloc(sizeof(char) * i);
-	if (!num)
+	num = n;
+	str = allocate_memory(num);
+	if (!str)
 		return (NULL);
-	i = 0;
-	printf("n: %d\n", n);
-	if (n > 9)
-		ft_itoa(n/10);
-	printf("After recursion\n");
-	num[i] = (n%10) + '0';
-	printf("%c\n",num[i]);
-	i++;
-	
-	return (num);
+	count = count_digits(num);
+	i = count;
+	res = fill_str(num, i, count, str);
+	return (res);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: akosmeni <akosmeni@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 20:42:34 by akosmeni          #+#    #+#             */
-/*   Updated: 2025/05/06 19:49:30 by akosmeni         ###   ########.fr       */
+/*   Updated: 2025/05/09 17:00:13 by akosmeni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,25 @@
 #include <limits.h>
 
 int main() {
-	char *s = ".Hel.,l.o,,,,";
-	char *ch = ",.";
-
-	char *res = ft_strtrim(s, ch);
+	char *res = ft_itoa(-23);
+	printf("Result: %s\n", res);
+	res = ft_itoa(-1);
+	printf("Result: %s\n", res);
+	res = ft_itoa(1);
+	printf("Result: %s\n", res);
+	res = ft_itoa(0);
+	printf("Result: %s\n", res);
+	res = ft_itoa(INT_MAX);
+	printf("Result: %s\n", res);
+	res = ft_itoa(INT_MIN);
+	printf("Result: %s\n", res);
+	res = ft_itoa(763767434);
+	printf("Result: %s\n", res);
+	res = ft_itoa(99999999);
+	printf("Result: %s\n", res);
+	res = ft_itoa(900909900);
+	printf("Result: %s\n", res);
+	res = ft_itoa(00);
 	printf("Result: %s\n", res);
     return 0;
 }
